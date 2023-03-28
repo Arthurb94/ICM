d170e8d7fb33ae97095266f7a137f125f4bfa342
Kathy Chhean <kathy.chhean@edu.esiee.fr>
2023-03-28 15:51:10 +0200

    countdown

--- mesure/image.py
+++ mesure/image.py
@@ -8,7 +8,35 @@ root = tk.Tk()
 root.title("Affichage d'images")
 root.geometry(f"{int(root.winfo_screenwidth())}x{int(root.winfo_screenheight())}")
 
+cross = Image.open('images/3.png')
+photo = ImageTk.PhotoImage(cross)
+label = tk.Label(root, image=photo)
+label.pack()
+root.update()
+time.sleep(1)
+cross.close()
+label.destroy()
+
+cross = Image.open('images/2.png')
+photo = ImageTk.PhotoImage(cross)
+label = tk.Label(root, image=photo)
+label.pack()
+root.update()
+time.sleep(1)
+cross.close()
+label.destroy()
+
+cross = Image.open('images/1.png')
+photo = ImageTk.PhotoImage(cross)
+label = tk.Label(root, image=photo)
+label.pack()
+root.update()
+time.sleep(1)
+cross.close()
+label.destroy()
+
 def show_image(image_path):
+
     cross = Image.open('images/cross.png')
     photo = ImageTk.PhotoImage(cross)
     label = tk.Label(root, image=photo)
@@ -28,11 +56,14 @@ def show_image(image_path):
     label.destroy()
 
 # Boucle pour afficher une image aléatoire toutes les 10 secondes pendant 5 minutes
+l = []
 for i in range(30):
     # Choix aléatoire d'une image
     image_path = random.choice(images)
     # Appel de la fonction pour afficher l'image
     show_image(image_path)
+    l.append(image_path)
 
 # Fermeture de la fenêtre Tkinter
 root.destroy()
+print(l)
